40b28d87b34f7e9b112f8d4736c329514b5ae947
Goochyr <richard@goochweb.org.uk>
2018-05-27 22:07:29 +0100

    Overhauled command system

--- modules/core/commandHandler.py
+++ modules/core/commandHandler.py
@@ -1,21 +1,34 @@
+import csv
+import shlex
+import os
 import modules.core.displaySong as dS
 import modules.core.setlistHandler as sH
 import modules.core.bibleHandler as bH
 import modules.core.screenControl as sC
 
-def doCommand(command):
-    sendCommand=command.split()
-    if sendCommand[0] == "bibleRef":
-        if len(sendCommand) == 5:
-            bibleRef(sendCommand[1]+sendCommand[2], sendCommand[3], sendCommand[4])
-        elif len(sendCommand) == 4:
-            bibleRef(sendCommand[1], sendCommand[2], sendCommand[3])
-    elif sendCommand[0] == "getBibles":
-        bH.getAvailableBibles()
-    elif sendCommand[0] == "saveSetlist":
-        sH.saveList(sendCommand[1])
+def init():
+    global commandTable
+    commandTable = {}
 
-def bibleRef(book, reference, translation):
-    bH.reference = book+" "+reference
-    bH.setTranslation(translation)
-    bH.createPassage()
+def buildCommandTable():
+    global commandTable
+    with open(os.getcwd()+"/modules/core/core.ullpref") as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            commandTable[row[0]] = [row[1], row[2], row[3]]
+    print(commandTable)
+
+def doCommand(incommand):
+    global commandTable
+    sendCommand=shlex.split(incommand)
+    command = sendCommand[0]
+    commandArgs = sendCommand[1:]
+    print(commandArgs)
+    returnString = ""
+    if command in commandTable:
+        if len(commandArgs) == int(commandTable[command][2]):
+            module = globals()[commandTable[command][0]]
+            result = getattr(module, str(commandTable[command][1]))(*commandArgs)
+        else:
+            returnString = "Wrong number of arguments"
+    return returnString
